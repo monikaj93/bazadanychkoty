e1d41e12e737f75f5049871c4e1f01e5b806201f
monikaj <monikaj9333@gmail.com>
2018-06-07 22:10:39 +0200

    Remove unecessery files.

--- Cot/koty-application/src/main/java/pl.kobietydokodu.bazakotow.model/KotDAO.java
+++ Cot/koty-application/src/main/java/pl.kobietydokodu.bazakotow.model/KotDAO.java
@@ -1,11 +1,12 @@
 package pl.kobietydokodu.bazakotow.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Repository
 public interface KotDao extends CrudRepository<Kot, Long> {
     public List<Kot> findBykotId(int kotId);
 }
--- Cot/koty-application/src/main/java/pl.kobietydokodu.bazakotow.model/ZabawkaDAO.java
+++ Cot/koty-application/src/main/java/pl.kobietydokodu.bazakotow.model/ZabawkaDAO.java
@@ -1,9 +1,10 @@
 package pl.kobietydokodu.bazakotow.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ZabawkaDAO extends CrudRepository<Zabawka, Long> {
         public List<Zabawka> findByzid(int zid);
     }
